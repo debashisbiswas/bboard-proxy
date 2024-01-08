6e746599404494eba7888bcf4b78d2b5aea4b2e9
Debashis Biswas <debashis.biswas@asu.edu>
2024-01-07 18:51:42 -0700

    Handle wrapping of long usernames

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,12 +37,31 @@
 				{post.title}
 			</h2>
 
-			<div class="flex items-center space-x-2 font-medium">
-				<div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-teal-400 text-sm">
+			<div class="font-medium">
+				<div
+					class="inline-flex items-center mr-1 rounded-full bg-teal-400/10 px-3 py-1 text-teal-400 text-sm"
+				>
 					{post.author}
 				</div>
-				<span class="text-slate-500 tracking-tight">
-					{formatDate(post.lastPost)} · {post.replies} replies
+
+				<span class="text-slate-500 tracking-tight inline-block">
+					<span class="mr-1">{formatDate(post.lastPost)}</span>
+
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-4 h-4 inline"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+						/>
+					</svg>
+					{post.replies}
 				</span>
 			</div>
 		</a>
